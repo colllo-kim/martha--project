e70c626bae0f349a5820093998a7b428226e4261
collo-kim <onemanlivity@gmail.com>
2025-08-10 17:06:22 +0300

    Your commit message new here

--- backened/server.js
+++ backened/server.js
@@ -32,7 +32,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // Routes
 app.use('/api/blogs', blogRouter);
 // Start server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`✅ Server is running on port ${port}`);
 });
